7ea744a052bdaba20d1625ab1e0aee2a9e91b8ae
Zhuoxuan Li <li.zhuoxua@northeastern.edu>
2026-01-07 23:24:57 -0500

    Reading atalas file

--- src/atlas_parser.py
+++ src/atlas_parser.py
@@ -1,5 +1,10 @@
 with open(r"C:\NonSYSFile\Gam\Mod\DarkestDungeon\ResolutionProject\atlas\anim\crusader.sprite.idle.atlas") as f:
     lines = f.readlines()
+    sprites = []
+    pngFile = lines[1].strip()
+    sheetWidth, sheetHeight = lines[2].split(":")[1].strip().split(",")
+    spriteSheet = {"sheetWidth" : int(sheetWidth), "sheetHeight" : int(sheetHeight)}
+
     # line.startswith(" ") to check for leading space
     # ":" in line to check if a line contains a colon
     # "." in line to check if a line contains a period
@@ -8,4 +13,18 @@ with open(r"C:\NonSYSFile\Gam\Mod\DarkestDungeon\ResolutionProject\atlas\anim\cr
             continue 
         elif not lines[index].startswith(" "):
             if "." not in lines[index] and ":" not in lines[index]:
-                print(index, lines[index])
+                if index + 3 > len(lines):
+                     break
+                else:
+                    x, y = lines[index + 2].split(":")[1].strip().split(", ")
+                    width, height = lines[index + 3].split(":")[1].strip().split(", ")
+                    sprite = {}
+                    sprite["name"] = lines[index].strip()
+                    sprite["x"] = int(x)
+                    sprite["y"] = int(y)
+                    sprite["width"] = int(width)
+                    sprite["height"] = int(height)
+                    sprites.append(sprite)
+    print(pngFile)
+    print(spriteSheet)
+    print(sprites)
